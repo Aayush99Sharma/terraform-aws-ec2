8adcd3b1edd3c62c1630b13da46c3faa3e329494
Aayush <ayush8276sharma@gmail.com>
2024-11-28 05:27:45 +0000

    first

--- main.tf
+++ main.tf
@@ -4,3 +4,14 @@ module "ec2" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
 }
+
+
+terraform {
+  backend "remote" {
+    organization = "aayush8276"
+    workspaces {
+      name = "terraform-cloud-task1"
+    }
+  }
+}
+
