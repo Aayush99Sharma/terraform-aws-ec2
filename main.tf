f3969cd4ef4bd51af82af0e6c4bae2a2aef4b785
Aayush Sharma <104816668+Aayush99Sharma@users.noreply.github.com>
2024-11-29 09:58:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 module "ec2" {
-  source        = "./modules/ec2"
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  source  = "app.terraform.io/aayush8276/ec2/aws"
+  version = "0.0.0"
+  ami           = ami-0c779fe4d45aced3d
+  instance_type = t2.medium
+  subnet_id     = subnet-0b6954ed0212537ff
+  AWS_ACCESS_KEY_ID=AKIAVYV5Z6NODRKVR4XQ
+  AWS_SECRET_ACCESS_KEY=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
 }
 
